c4159e93df9b593783e610e3c351999ecb9bd5c7
yoonhero <yoonhero06@naver.com>
2023-07-21 23:08:05 +0900

    fix: It's working now?!

--- app/test.py
+++ app/test.py
@@ -13,39 +13,6 @@ import time
 torch.set_grad_enabled(False)
 torch.set_float32_matmul_precision("high")
 
-bootstrap = os.environ["KAFKA_BOOTSTRAP_ADDRESS"]
-req = os.environ["KAFKA_REQ_TOPIC"]
-RESP_TOPIC = os.environ["KAFKA_RESP_TOPIC"]
-username = os.environ["KAFKA_USERNAME"]
-password = os.environ["KAFKA_PASSWORD"]
-node_id = os.environ["NODE_ID"]
-
-model_name = os.environ["MODEL_NAME"]
-model_url = os.environ["MODEL_URL"]
-max_batch_item = int(os.environ["MAX_BATCH_SIZE"])
-
-print("model download start")
-start = time.time()
-wget.download(model_url, out="model_store/jamo.tar")
-print("model download success in", time.time()-start)
-# target_file = glob.glob("model_store/*.tar")[0]
-# new_filename = "model_store/jamo.tar"
-# os.rename(target_file, new_filename)
-
-consumer = KafkaConsumer(req, group_id=f'inf-{model_name}',
-                         client_id=f'inference-cons-{node_id}',
-                         bootstrap_servers=bootstrap,
-                         security_protocol="SASL_PLAINTEXT",
-                         sasl_mechanism="SCRAM-SHA-512",
-                         sasl_plain_username=username,
-                         sasl_plain_password=password)
-producer = KafkaProducer(bootstrap_servers=bootstrap,
-                         client_id=f'inference-prod-{node_id}',
-                         security_protocol="SASL_PLAINTEXT",
-                         sasl_mechanism="SCRAM-SHA-512",
-                         sasl_plain_username=username,
-                         sasl_plain_password=password)
-
 class Jamo():
     def __init__(self):
         self.jamo = JamoService()
@@ -189,17 +156,9 @@ class Jamo():
 
         return cleaned_answer
 
-class GracefulKiller:
-  kill_now = False
-  def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
-
-  def exit_gracefully(self, *args):
-    self.kill_now = True
-
 model = Jamo()
 print("Load the model")
+
 
 print("?")
 with open("/tmp/healthy", "w") as f:
@@ -218,8 +177,7 @@ def set_timeout(seconds):
     signal.signal(signal.SIGALRM, timeout_handler)
     signal.alarm(seconds)
 
-killer = GracefulKiller()
-while not killer.kill_now:
+while True:
     # GET requests
     req_ids = []
     reqs = []
@@ -238,75 +196,18 @@ while not killer.kill_now:
     poll_time = 50
 
     # for msg in consumer:
-    for _ in range(int(target_time//poll_time)):
-        # {TopicPartition(topic='model-req', partition=0): [ConsumerRecord(topic='model-req', partition=0, offset=110, timestamp=1689869876532, timestamp_type=0, key=None, value=b'{"req":"\xec\x98\xa4","context":[],"stream":true,"max_token":256,"temperature":0.8,"top_k":15}', headers=[('target_model', b'prod_a'), ('req_id', b'6640494b-859a-4eb0-a482-5c11debdc562'), ('__TypeId__', b'io.teammistake.suzume.data.InferenceRequest')], checksum=None, serialized_key_size=-1, serialized_value_size=85, serialized_header_size=113)]}
-        if len(req_ids) >= max_batch_item or time.time() - start > target_time/1000:
-            print("timeout!")
-            break
-        records = consumer.poll(50, max_records=1)
-        if records is None or records == {}: continue
-
-        for i in records.items():
-            msg = i[1][0]
-            break
-
-        headerList = msg.headers
-        model_match = False
-        req_id = ''
-        for k, v in headerList:
-            if k == "target_model":
-                if v.decode() == model_name:
-                    model_match = True
-            if k == "req_id":
-                req_id = v.decode()
-        if not model_match:
-            continue
-
-        content = msg.value.decode()
-
-        parsed = json.loads(content)
-
-        if stream == None:
-            stream = parsed['stream']
-            max_token = parsed["max_token"]
-            temperature = parsed["temperature"]
-            top_k: int = parsed["top_k"]
-        elif stream != parsed["stream"]:
-            print(stream, parsed["stream"])
-            continue
-        req = parsed['req']
-        reqs.append(req)
-        req_ids.append(req_id)
-        print("get req")
-    # except TimeoutError:
-    #     print("timeout error")
-    #     # Handle the timeout exception
-    #     if len(req_ids) == 0:
-    #         continue
-    # else: 
-    #     print("WHY?")        
-    #     continue
-
-    if len(req_ids)==0:
-        continue
+    
 
     print("Generation Start")
     if stream:
         print("Streaming Creation")
-
-        for req_id in req_ids:
-            producer.send(RESP_TOPIC, json.dumps({
-                'resp_partial': "",
-                'resp_full': "",
-                'eos': False
-            }).encode(), headers=[("req_id", req_id.encode()), ("seq_id", '1'.encode())])
-
         seq = 1
         already = list(range(len(reqs)))
         done = [0] * len(already)
         total = {key:"" for key in already}
-        for (parts, eoses) in model.generator(reqs, max_token=max_token, temperature=temperature, top_k=top_k):
+        for (parts, eoses) in model.generator(req, max_token=max_token, temperature=temperature, top_k=top_k):
             seq += 1
+            print(parts, eoses)
             for target_index in already:
                 if done[target_index] != 1:
                     part, eos = parts[target_index], eoses[target_index]
